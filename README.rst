69af11606f7b1fda523603b75931dda9f20cb1ab
Michel Zou <xantares09@hotmail.com>
2017-07-07 14:28:27 +0200

    initial import

---
+++ README.rst
@@ -0,0 +1,10 @@
+======================
+CMake support for FFTW
+======================
+
+.. image:: https://travis-ci.org/xantares/fftw-cmake.svg?branch=master
+    :target: https://travis-ci.org/xantares/fftw-cmake
+
+.. image:: https://ci.appveyor.com/api/projects/status/1jsdukvfg2tequ36?svg=true
+    :target: https://ci.appveyor.com/project/xantares/fftw-cmake/branch/master
+
---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+wget -c http://fftw.org/fftw-3.3.6-pl2.tar.gz
+rm -rf /tmp/fftw-3.3.6-pl2
+tar xzf fftw-3.3.6-pl2.tar.gz -C /tmp
+# cp -r /tmp/fftw-3.3.6 /tmp/fftw-3.3.6.copy && pushd /tmp/fftw-3.3.6.copy && ./configure && cat config.h && make && popd
+cp -r * /tmp/fftw-3.3.6-pl2
+cd /tmp/fftw-3.3.6-pl2
+cmake -DCMAKE_INSTALL_PREFIX=~/.local -DENABLE_SSE2=ON -DENABLE_THREADS=ON -DENABLE_OPENMP=ON -DENABLE_FLOAT=ON .
+# cat config.h
+make VERBOSE=1 install -j2 && make tests && ctest
