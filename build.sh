0c3ae9f7468e87579156e5bdf601dbb14abc82a8
Michel Zou <xantares09@hotmail.com>
2017-07-09 13:08:15 +0200

    add BUILD_TESTS

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ cp -r * /tmp/fftw-3.3.6-pl2
 cd /tmp/fftw-3.3.6-pl2
 cmake -DCMAKE_INSTALL_PREFIX=~/.local -DENABLE_THREADS=ON -DENABLE_OPENMP=ON -DENABLE_FLOAT=ON -DENABLE_LONG_DOUBLE=OFF -DENABLE_QUAD_PRECISION=OFF -DENABLE_SSE2=ON .
 # cat config.h
-make VERBOSE=1 install -j2 && make tests VERBOSE=1 && ctest
+make VERBOSE=1 install -j2 && ctest
