6cb998b1cae764b7289806d343ca6e78289d9ffd
Michel Zou <xantares09@hotmail.com>
2017-07-09 13:17:24 +0200

    Clear cache

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ tar xzf fftw-3.3.6-pl2.tar.gz -C /tmp
 # cp -r /tmp/fftw-3.3.6 /tmp/fftw-3.3.6.copy && pushd /tmp/fftw-3.3.6.copy && ./configure && cat config.h && make && popd
 cp -r * /tmp/fftw-3.3.6-pl2
 cd /tmp/fftw-3.3.6-pl2
-cmake -DCMAKE_INSTALL_PREFIX=~/.local -DENABLE_THREADS=ON -DENABLE_OPENMP=ON -DENABLE_FLOAT=ON -DENABLE_LONG_DOUBLE=OFF -DENABLE_QUAD_PRECISION=OFF -DENABLE_SSE2=ON .
+cmake -DCMAKE_INSTALL_PREFIX=~/.local -DENABLE_THREADS=ON -DENABLE_OPENMP=ON -DENABLE_FLOAT=ON -DENABLE_LONG_DOUBLE=ON -DENABLE_QUAD_PRECISION=OFF -DENABLE_SSE2=OFF .
 # cat config.h
 make VERBOSE=1 install -j2 && ctest
