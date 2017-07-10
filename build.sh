4ae5200747d759f6408326cd57707d5eef1f3b6c
Michel Zou <xantares09@hotmail.com>
2017-07-11 00:55:16 +0200

    various fixes

--- build.sh
+++ build.sh
@@ -4,8 +4,12 @@ wget -c http://fftw.org/fftw-3.3.6-pl2.tar.gz
 rm -rf /tmp/fftw-3.3.6-pl2
 tar xzf fftw-3.3.6-pl2.tar.gz -C /tmp
 # cp -r /tmp/fftw-3.3.6 /tmp/fftw-3.3.6.copy && pushd /tmp/fftw-3.3.6.copy && ./configure && cat config.h && make && popd
-cp -r * /tmp/fftw-3.3.6-pl2
 cd /tmp/fftw-3.3.6-pl2
-cmake -DCMAKE_INSTALL_PREFIX=~/.local -DENABLE_THREADS=ON -DENABLE_OPENMP=ON -DENABLE_FLOAT=ON -DENABLE_LONG_DOUBLE=OFF -DENABLE_QUAD_PRECISION=OFF -DENABLE_SSE2=ON -DENABLE_AVX=ON .
+./configure --enable-float --enable-sse2 --enable-avx --enable-threads
+mv config.h config.h.autotools
+cd -
+cp * /tmp/fftw-3.3.6-pl2
+cd /tmp/fftw-3.3.6-pl2
+cmake -DCMAKE_INSTALL_PREFIX=~/.local -DENABLE_THREADS=ON -DENABLE_OPENMP=OFF -DENABLE_FLOAT=ON -DENABLE_LONG_DOUBLE=OFF -DENABLE_QUAD_PRECISION=OFF -DENABLE_SSE2=ON -DENABLE_AVX=ON .
 # cat config.h
 make VERBOSE=1 install -j2 && ctest
